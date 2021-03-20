4f24d3cc32bd8116f4d48cafe4309c02270c11e3
XcodeOfNHJ <1217423985@qq.com>
2021-03-20 16:50:37 +0900

    150

--- EvaluateReversePolishNotation/EvaluateReversePolishNotation/main.cpp
+++ EvaluateReversePolishNotation/EvaluateReversePolishNotation/main.cpp
@@ -1,14 +1,79 @@
-//
-//  main.cpp
-//  EvaluateReversePolishNotation
-//
-//  Created by 王萱 on 2021/3/20.
-//
+/*
+EvaluateReversePolishNotation
+by N.H.J.
+2021.3.20
+*/
 
-#include <iostream>
+class Solution {
+public:
+    int arrarys[10000];
+    int length=0;
+    int numlength=0;
+    
+    int evalRPN(vector<string>& tokens) {
+        
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
-}
+        for(int i=0;i<tokens.size();i++)
+        {
+
+            if(tokens[i]=="+")
+            {
+                inStack(-201);
+            }
+            else if(tokens[i]=="-")
+            {
+                inStack(-202);
+            }
+            else if(tokens[i]=="*")
+            {
+                inStack(-203);
+            }
+            else if(tokens[i]=="/")
+            {
+                inStack(-204);
+            }
+            else
+            {
+                inStack(stoi(tokens[i]));
+            }
+                
+        }
+        
+        return arrarys[0];
+
+    }
+    void inStack(int a)
+    {
+        if(a>=-200)
+        {
+            numlength++;
+            arrarys[length]=a;
+            length++;
+        }
+        else
+        {
+            if(a==-201)
+            {
+                arrarys[length-2]=arrarys[length-2]+arrarys[length-1];
+                length=length-1;
+            }
+            else if(a==-202)
+            {
+                arrarys[length-2]=arrarys[length-2]-arrarys[length-1];
+                length=length-1;
+            }
+            else if(a==-203)
+            {
+                arrarys[length-2]=arrarys[length-2]*arrarys[length-1];
+                length=length-1;
+            }
+            else if(a==-204)
+            {
+                arrarys[length-2]=arrarys[length-2]/arrarys[length-1];
+                length=length-1;
+            }
+        }
+    }
+};
+
+
