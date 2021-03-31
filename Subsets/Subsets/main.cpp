d6f73215e1c623cdc2f92ee1dcd4367b2f5f6d5b
XcodeOfNHJ <1217423985@qq.com>
2021-03-31 17:56:41 +0900

    init

--- Subsets/Subsets/main.cpp
+++ Subsets/Subsets/main.cpp
@@ -4,9 +4,24 @@ by N.H.J.
 2021.3.31
 ver.1
 */
+
 class Solution {
 public:
+    vector<int> t;
+    vector<vector<int>> ans;
     vector<vector<int>> subsets(vector<int>& nums) {
+
+    int n = nums.size();
+        for (int mask = 0; mask < (1 << n); ++mask) {
+            t.clear();
+            for (int i = 0; i < n; ++i) {
+                if (mask & (1 << i)) {
+                    t.push_back(nums[i]);
+                }
+            }
+            ans.push_back(t);
+        }
+    return ans;
 
     }
 };
